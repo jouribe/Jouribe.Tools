b2e239c1518b5f6faadadd14da3142193d32d05b
Jorge O. Uribe <jouribepe@gmail.com>
2020-10-06 20:59:02 -0500

    Add project files.

---
+++ Jouribe.Tools.MassInserts.Application/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Jouribe.Tools.MassInserts.Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Jouribe.Tools.MassInserts.Application/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Jouribe.Tools.MassInserts.Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
